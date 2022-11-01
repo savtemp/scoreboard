993773eafc6628911b0af3cfa6028c7ef9a0cb41
Savannah Temple <savtemple@gmail.com>
2022-11-01 09:36:08 -0600

    notes for attempt function explanation

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-let homeScore = 0
-let awayScore = 0
+let homeScore = 0;
+let awayScore = 0;
 
-function clearHome(){
-    homeScore = 0
-    let scoreElm = document.getElementById('home-score')
-    scoreElm.innerText = homeScore
+function clearHome() {
+  homeScore = 0;
+  let scoreElm = document.getElementById("home-score");
+  scoreElm.innerText = homeScore;
 }
 
-function clearAway(){
-    awayScore = 0
-    let scoreElm = document.getElementById('away-score')
-    scoreElm.innerText = awayScore
+function clearAway() {
+  awayScore = 0;
+  let scoreElm = document.getElementById("away-score");
+  scoreElm.innerText = awayScore;
 }
 
-function HomeAddOne(){
-    homeScore += 1
-    console.log(homeScore);
-    let scoreElm = document.getElementById('home-score')
-    scoreElm.innerText = homeScore
-    console.log(homeScore)
+function HomeAddOne() {
+  homeScore += 1;
+  console.log(homeScore);
+  let scoreElm = document.getElementById("home-score");
+  scoreElm.innerText = homeScore;
+  console.log(homeScore);
 }
 
-function AwayAddOne(){
-    awayScore += 1
-    console.log(awayScore);
-    let scoreElm = document.getElementById('away-score')
-    scoreElm.innerText = awayScore
+function AwayAddOne() {
+  awayScore += 1;
+  console.log(awayScore);
+  let scoreElm = document.getElementById("away-score");
+  scoreElm.innerText = awayScore;
 }
 
-function HomeAddThree(){
-    homeScore += 3
-    console.log(homeScore);
-    let scoreElm = document.getElementById('home-score')
-    scoreElm.innerText = homeScore
+function HomeAddThree() {
+  homeScore += 3;
+  console.log(homeScore);
+  let scoreElm = document.getElementById("home-score");
+  scoreElm.innerText = homeScore;
 }
 
-function AwayAddThree(){
-    awayScore += 3
-    console.log(awayScore);
-    let scoreElm = document.getElementById('away-score')
-    scoreElm.innerText = awayScore
+function AwayAddThree() {
+  awayScore += 3;
+  console.log(awayScore);
+  let scoreElm = document.getElementById("away-score");
+  scoreElm.innerText = awayScore;
 }
+
+// NOTE have 1 button that randomly chooses whether the home or the away team score increase by 1
+// function increaseScore(){
+//     let score = 0
+//     if(score = 0){
+//         Math.random(scoreElm = getElementById("random-score"))
+//     } else {
+
+//     }
+//     score ++
+// }
